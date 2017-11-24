599cdb5e158db85b1a31e10cd3bf665968f6c2b6
henylee <asqaws1@gmail.com>
2017-11-24 18:17:38 +0900

    http://applandeo.com/blog/material-calendar-view-customized-calendar-widget-android/

--- app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
@@ -1,27 +1,20 @@
 package tje.co.kr.schedulerhousekeeping;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import com.applandeo.materialcalendarview.CalendarView;
 
 public class MainActivity extends BaseActivity {
 
-    private com.github.sundeepk.compactcalendarview.CompactCalendarView compactcalendarview;
+    private com.applandeo.materialcalendarview.CalendarView calendarView;
+    private android.support.design.widget.FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
+        this.floatingActionButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.floatingActionButton);
+        this.calendarView = (CalendarView) findViewById(R.id.calendarView);
 
         bindViews();
         setupEvents();
@@ -30,30 +23,16 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-        compactcalendarview.setListener(new CompactCalendarView.CompactCalendarViewListener() {
 
-            @Override
-            public void onDayClick(Date dateClicked) {
-                CompactCalendarTab cct;
-                List<Event> events = compactcalendarview.getEvents(dateClicked);
-                Log.d(TAG, "Day was clicked: " + dateClicked + " with events " + events);
-            }
-
-            @Override
-            public void onMonthScroll(Date firstDayOfNewMonth) {
-                Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
-            }
-        });
     }
 
     @Override
     public void setValues() {
-        compactcalendarview.setFirstDayOfWeek(Calendar.MONDAY);
+
     }
 
     @Override
     public void bindViews() {
-        this.compactcalendarview = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
 
     }
 }
