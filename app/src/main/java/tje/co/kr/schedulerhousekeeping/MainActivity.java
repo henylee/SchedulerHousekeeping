c84dc36083a08250167e7cad9ede618dae5f1636
henylee <asqaws1@gmail.com>
2017-11-24 18:21:23 +0900

    http://applandeo.com/blog/material-calendar-view-customized-calendar-widget-android/

--- app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
@@ -1,20 +1,29 @@
 package tje.co.kr.schedulerhousekeeping;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
     private com.applandeo.materialcalendarview.CalendarView calendarView;
     private android.support.design.widget.FloatingActionButton floatingActionButton;
+    public static final String RESULT = "result";
+    public static final String EVENT = "event";
+    private static final int ADD_NOTE = 44;
+    private List<EventDay> mEventDays = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.floatingActionButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.floatingActionButton);
-        this.calendarView = (CalendarView) findViewById(R.id.calendarView);
 
         bindViews();
         setupEvents();
@@ -23,7 +32,18 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNote();
+            }
+        });
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                previewNote(eventDay);
+            }
+        });
     }
 
     @Override
@@ -33,6 +53,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.floatingActionButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.floatingActionButton);
+        this.calendarView = (CalendarView) findViewById(R.id.calendarView);
     }
 }
