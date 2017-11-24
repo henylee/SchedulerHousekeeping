6d2557483057f0351dacfdc781d44a2e2bc16f42
cho881020 <cho881020@gmail.com>
2017-11-24 17:54:59 +0900

    이벤트 목록 찍어주기

--- app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
@@ -7,21 +7,23 @@ import android.util.Log;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends BaseActivity {
+    public static final String TAG = "MainActivity";
 
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
     private com.github.sundeepk.compactcalendarview.CompactCalendarView compactcalendarview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
 
         bindViews();
         setupEvents();
@@ -48,12 +50,75 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        loadEvents();
+        loadEventsForYear(2017);
         compactcalendarview.setFirstDayOfWeek(Calendar.MONDAY);
+        Event ev1 = new Event(Color.GREEN, Calendar.getInstance().getTimeInMillis(), "Some extra data that I want to store.");
+
+        compactcalendarview.addEvent(ev1);
+
     }
 
     @Override
     public void bindViews() {
         this.compactcalendarview = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
 
+    }
+
+    private List<Event> getEvents(long timeInMillis, int day) {
+        if (day < 2) {
+            return Arrays.asList(new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)));
+        } else if ( day > 2 && day <= 4) {
+            return Arrays.asList(
+                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)),
+                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)));
+        } else {
+            return Arrays.asList(
+                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis) ),
+                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)),
+                    new Event(Color.argb(255, 70, 68, 65), timeInMillis, "Event 3 at " + new Date(timeInMillis)));
+        }
+    }
+
+    private void loadEvents() {
+        addEvents(-1, -1);
+        addEvents(Calendar.DECEMBER, -1);
+        addEvents(Calendar.AUGUST, -1);
+    }
+
+    private void loadEventsForYear(int year) {
+        addEvents(Calendar.NOVEMBER, year);
+        addEvents(Calendar.AUGUST, year);
+    }
+
+
+    private void addEvents(int month, int year) {
+        currentCalender.setTime(new Date());
+        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
+        Date firstDayOfMonth = currentCalender.getTime();
+        for (int i = 0; i < 6; i++) {
+            currentCalender.setTime(firstDayOfMonth);
+            if (month > -1) {
+                currentCalender.set(Calendar.MONTH, month);
+            }
+            if (year > -1) {
+                currentCalender.set(Calendar.ERA, GregorianCalendar.AD);
+                currentCalender.set(Calendar.YEAR, year);
+            }
+            currentCalender.add(Calendar.DATE, i);
+            setToMidnight(currentCalender);
+            long timeInMillis = currentCalender.getTimeInMillis();
+
+            List<Event> events = getEvents(timeInMillis, i);
+
+            compactcalendarview.addEvents(events);
+        }
+    }
+
+    private void setToMidnight(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
     }
 }
