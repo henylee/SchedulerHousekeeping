cc26b56a2922203546a1a59c1e08355cf69875cf
asqaw <asqaws1@gmail.com>
2017-11-28 15:44:48 +0900

    문자 서비스 연동중

---
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MySMSReceiver.java
@@ -0,0 +1,15 @@
+package tje.co.kr.schedulerhousekeeping;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MySMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
