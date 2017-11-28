b439d9b57e175941783b8b9c3a879e5ca910eeef
asqaw <asqaws1@gmail.com>
2017-11-28 11:12:24 +0900

    드로우레이아웃 1차완료!!!

--- app/src/main/java/tje/co/kr/schedulerhousekeeping/BaseActivity.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/BaseActivity.java
@@ -1,7 +1,10 @@
 package tje.co.kr.schedulerhousekeeping;
 
 import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 /**
  * Created by joeun on 2017-11-22.
@@ -11,8 +14,20 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     Context mContext=this;
 
+    public ImageView hamburgBtn;
+
     public abstract void setupEvents();
     public abstract void setValues();
     public abstract void bindViews();
+
+    public void setCustomActionBar() {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        hamburgBtn = getSupportActionBar().getCustomView().findViewById(R.id.hamburgBtn);
+        Toolbar parent = (Toolbar) getSupportActionBar().getCustomView().getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+        getSupportActionBar().setElevation(0);
+    }
 
 }
--- app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MainActivity.java
@@ -3,7 +3,17 @@ package tje.co.kr.schedulerhousekeeping;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.rd.PageIndicatorView;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
@@ -20,11 +30,34 @@ public class MainActivity extends BaseActivity {
     private com.applandeo.materialcalendarview.CalendarView calendarView;
     private android.support.design.widget.FloatingActionButton floatingActionButton;
     private List<EventDay> mEventDays = new ArrayList<>();
+    private android.widget.ImageView myProfileImg;
+    private android.widget.EditText idEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.CheckBox autoLoginCBox;
+    private android.widget.TextView loginBtn;
+    private android.widget.TextView signUpBtn;
+    private android.widget.LinearLayout beforeLoginLayout;
+    private android.widget.TextView cashTxt;
+    private android.widget.ImageView closeBtn2;
+    private android.widget.LinearLayout noTicketLayout;
+    private android.support.v4.view.ViewPager ticketViewPager;
+    private com.rd.PageIndicatorView pageIndicatorView;
+    private android.widget.LinearLayout userTicketLayout;
+    private android.widget.TextView phoneNumTxt;
+    private android.widget.TextView emailTxt;
+    private android.widget.ImageView imageView;
+    private android.widget.TextView carNumTxt;
+    private android.widget.ExpandableListView expandMenuListView;
+    private android.widget.LinearLayout serviceLayout2;
+    private android.widget.LinearLayout afterLoginLayout;
+    private android.widget.LinearLayout llll;
+    private android.support.v4.widget.DrawerLayout dlactivitymaindrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCustomActionBar();
 
         bindViews();
         setupEvents();
@@ -33,6 +66,14 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        hamburgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dlactivitymaindrawer.openDrawer(llll);
+            }
+        });
+
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +86,22 @@ public class MainActivity extends BaseActivity {
                 previewNote(eventDay);
             }
         });
+
+        llll.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent Event) {
+                if (Event.getAction()==MotionEvent.ACTION_DOWN) {
+                    return true;
+                }
+                else if (Event.getAction()==MotionEvent.ACTION_MOVE) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -75,6 +132,28 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.dlactivitymaindrawer = (DrawerLayout) findViewById(R.id.dl_activity_main_drawer);
+        this.llll = (LinearLayout) findViewById(R.id.llll);
+        this.afterLoginLayout = (LinearLayout) findViewById(R.id.afterLoginLayout);
+        this.serviceLayout2 = (LinearLayout) findViewById(R.id.serviceLayout2);
+        this.expandMenuListView = (ExpandableListView) findViewById(R.id.expandMenuListView);
+        this.carNumTxt = (TextView) findViewById(R.id.carNumTxt);
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+        this.emailTxt = (TextView) findViewById(R.id.emailTxt);
+        this.phoneNumTxt = (TextView) findViewById(R.id.phoneNumTxt);
+        this.userTicketLayout = (LinearLayout) findViewById(R.id.userTicketLayout);
+        this.pageIndicatorView = (com.rd.PageIndicatorView) findViewById(R.id.pageIndicatorView);
+        this.ticketViewPager = (ViewPager) findViewById(R.id.ticketViewPager);
+        this.noTicketLayout = (LinearLayout) findViewById(R.id.noTicketLayout);
+        this.closeBtn2 = (ImageView) findViewById(R.id.close_Btn2);
+        this.cashTxt = (TextView) findViewById(R.id.cashTxt);
+        this.beforeLoginLayout = (LinearLayout) findViewById(R.id.beforeLoginLayout);
+        this.signUpBtn = (TextView) findViewById(R.id.signUpBtn);
+        this.loginBtn = (TextView) findViewById(R.id.loginBtn);
+        this.autoLoginCBox = (CheckBox) findViewById(R.id.autoLoginCBox);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
+        this.myProfileImg = (ImageView) findViewById(R.id.myProfileImg);
         this.floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         this.calendarView = (CalendarView) findViewById(R.id.calendarView);
     }
