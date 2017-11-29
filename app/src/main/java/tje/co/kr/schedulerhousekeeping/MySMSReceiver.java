1027b217ce18369e9b1015f95caa77c577bd9bc7
asqaw <asqaws1@gmail.com>
2017-11-29 10:48:03 +0900

    문자 서비스 연동중

--- app/src/main/java/tje/co/kr/schedulerhousekeeping/MySMSReceiver.java
+++ app/src/main/java/tje/co/kr/schedulerhousekeeping/MySMSReceiver.java
@@ -3,13 +3,33 @@ package tje.co.kr.schedulerhousekeeping;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class MySMSReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Bundle bundle = intent.getExtras();
+
+        if (bundle!=null) {
+            Object [] pdus = (Object[]) bundle.get("pdus");
+
+            SmsMessage[] msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+                String sender = msgs[i].getOriginatingAddress();
+                String content = msgs[i].getMessageBody().toString();
+
+                if (sender.equals("16449999")) {
+
+                }
+            }
+        }
+
     }
 }
